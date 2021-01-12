d9d7e2677aa4cdbb7426937fc450f589c4c0b4df
Orlando Castillo <omaddoc@gmail.com>
2021-01-12 04:26:45 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -47,13 +47,30 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
 // Links
-const nav = document.querySelectorAll("nav");
+const nav = document.querySelector("nav");
 let navLinks = document.querySelectorAll("nav a");
 // navLinks[0].textContent = siteContent["nav"]["nav-item-0"];
 navLinks.forEach(
   (item, index) =>
     (item.textContent = siteContent["nav"]["nav-item-" + (index + 1)])
 );
+navLinks.forEach((item) => (item.style.color = "green"));
+
+// CREATE AND APPEND
+const newEl1 = document.createElement("a");
+const newEl2 = document.createElement("a");
+newEl1.classList.add("nav-item-7");
+newEl2.classList.add("nav-item-8");
+newEl1.href = "#";
+newEl2.href = "#";
+newEl1.textContent = "Fire";
+newEl2.textContent = "Water";
+newEl1.style.color = "red";
+newEl2.style.color = "blue";
+// console.log(newEl1);
+// console.log(newEl2);
+nav.appendChild(newEl1);
+nav.appendChild(newEl2);
 
 const ctaText = document.querySelector(".cta-text");
 const ctaH1 = document.querySelector(".cta-text h1");
@@ -68,8 +85,15 @@ const topContent = document.querySelector(".top-content");
 const textContent = document.querySelector(".text-content");
 const textContentH4 = document.querySelectorAll(".text-content h4");
 const textContentP = document.querySelectorAll(".text-content p");
+const middleImg = document.querySelector(".middle-img");
+const contact = document.querySelector(".contact");
+const contactH4 = document.querySelector(".contact h4");
+const contactP = document.querySelectorAll(".contact p");
+const footer = document.querySelector("footer");
+const footerP = document.querySelector("footer p");
 
 const arrMid = ["features", "about", "services", "product", "vision"];
+const contactInfo = ["address", "phone", "email"];
 
 // textContentH4[0].textContent = siteContent["main-content"]["features-h4"];
 // textContentH4[1].textContent = siteContent["main-content"]["about-h4"];
@@ -85,3 +109,13 @@ textContentP.forEach(
   (item, index) =>
     (item.textContent = siteContent["main-content"][arrMid[index] + "-content"])
 );
+
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+contactP.forEach(
+  (item, index) =>
+    (item.textContent = siteContent["contact"][contactInfo[index]])
+);
+
+footerP.textContent = siteContent["footer"]["copyright"];
